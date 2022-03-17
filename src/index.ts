2d0ca6ad2e20874e9b203247a53c91ebc81ef4b8
Yashwanth Javvaji <yashwanthjavvaji208@gmail.com>
2022-03-18 00:39:13 +0530

    updates

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ export * from './decorators/public.decorator';
 export * from './dto/payload-with-refresh-token.dto';
 export * from './dto/payload.dto';
 
+//Enums
+export * from './enums/roles.enum';
+
 // Guards
 export * from './guards/jwt-access-token-auth.guard';
 export * from './guards/jwt-refresh-token-auth.guard';
